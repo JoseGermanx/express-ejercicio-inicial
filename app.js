602c53a053abaaf0afb6b76cc527d5295555fb09
Jose Germán Martínez <jgermanmm@gmail.com>
2023-11-06 12:59:27 -0300

    Middlewares

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
@@ -7,12 +6,28 @@ const port = 8080;
 
 // crear una ruta (taquilla del banco)
 
+app.use((req, res, next) => {
+  console.log(`De paso por este middleware el ${new Date()}`);
+  next();
+});
+
+// autenticacion basica
+app.use((req, res, next) => {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader) {
+    res.status(401).json({
+      msg: "Autorizacion requerida",
+    });
+  } else {
+    next();
+  }
+});
+
 // primer parametro la funcion controladora es el requerimiendo de la peticion (req, request)
-app.get('/taquilla-uno', (req, res) => res.send('Hola desde la taquilla uno') )
+app.get("/taquilla-uno", (req, res) => res.send("Hola desde la taquilla uno"));
 //             ruta                          funcion controladora
 
-app.get('/taquilla-dos', (req, res) => res.send('Hola desde la taquilla dos') )
+app.get("/taquilla-dos", (req, res) => res.send("Hola desde la taquilla dos"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
